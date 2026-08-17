3126df867a1860836491a6973928169b9a202631
Kareemot <abdulazeezkareemot88@gmail.com>
2026-08-17 14:43:43 +0100

    Fix Vercel Express deployment

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname)));
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
 
 // ===============================
 // SUPABASE
